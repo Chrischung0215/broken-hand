31c2b96bdf89593aa7300bf149b822bff4d6a7b4
Chis_0215 <chris.chung0215@gmail.com>
2025-06-12 00:52:28 +0800

    final

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         sys.exit(1)
 
     filename = sys.argv[1]
-    with open(filename, 'r') as f:
+    with open(filename, 'r',encoding='utf-8') as f:
         code = f.read()
 
     try:
